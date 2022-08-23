c16edc696965e6863dfe12e05e8fcdcc4ca75e66
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-08-23 10:49:51 -0300

    alterações de estrutura

--- index.php
+++ index.php
@@ -20,35 +20,32 @@
 ?>  
 <header id="header_cabec"><!-- GRID ITEM cabecalho -->
         <div id="div_text_esq">
-            Uma breve descrição do site!
+            <img id="img_logo_grand" src="imgs/Logo_Dtudo_2022-300p.png">
         </div>
         <div id="div_img_logo">
-            <img id="img_logo_grand" src="imgs/DTUDO-Logo_2021_303x100.jpg">
+            
         </div>
         <div id="div_text_dir">
-            Mais Informações estaram aqui!
+            Mais Informações estaram aqui!<br>
+            Uma breve descrição do site!
         </div>
 </header>
 <main id="main_princ"><!--GRID ITEM e também um GRID CONTEINER-->
 <!--    <img id="img_fundo_main" src="imgs/software_1200px.jpg">-->
         <div class="divs_main" id="div_img_bitcoin" onMouseOver="mudaFoto('imgs/bitcoin_text_centro.png')" onMouseOut="mudaFoto('imgs/terra_250px.png')" >
-            <a href="https://localhost/dtudo/bitcoin.php" target="_new" title="Pagina sobre Bitcoin">
-                <img src="imgs/bitcoin_logo_text_300.png" id="img_bitcoin" title="Informação Sobre CriptoAtivos em Geral"></a>
+            
         </div>
         <div class="divs_main" id="div_img_ti" onMouseOver="mudaFoto('imgs/TI_text_centro.png')" onMouseOut="mudaFoto('imgs/terra_250px.png')">
             <a href="https://localhost/dtudo/t_i.php" target="_new" title="Pagina sobre Tecnologia da Informação">
                 <img src="imgs/TI_link.png" id="img_ti" title="Informação sobre Tecnologia da Informação"></a>
         </div>
         <div class="divs_main" id="div_img_centro" >
-<!--            <img src="imgs/terra_250px.png" alt="Terra imagem centro" id="img_centro">-->
-        </div>
-        <div class="divs_main" id="div_img_animes" onMouseOver="mudaFoto('imgs/animes_text_centro.png')" onMouseOut="mudaFoto('imgs/terra_250px.png')">
-            <a href="/lazer/animacao/https://localhost/dtudo/animacao.php" target="_new" title="Pagina sobre Animes em Geral">
-                <img src="imgs/animes_personagem_400px.png" id="img_animes" title="Todos os Animes que já assisti"> </a>
+            <a href="https://localhost/dtudo/bitcoin.php" target="_new" title="Pagina sobre Bitcoin">
+            <img src="imgs/SlaveMoney.png" id="img_bitcoin" title="Informação Sobre CriptoAtivos em Geral"></a>
         </div>
         <div class="divs_main" id="div_img_musicas" onMouseOver="mudaFoto('imgs/musica_text_centro.png')" onMouseOut="mudaFoto('imgs/terra_250px.png')">
-            <a href="https://localhost/dtudo/musica.php" target="_new" title="Pagina sobre Musica">
-                <img src="imgs/NotaMusica_200px.png" id="img_musicas"></a>
+            <a href="https://localhost/dtudo/lazer/animacao/animacao.php" target="_new" title="Pagina sobre Animes em Geral">
+                <img src="imgs/animes_personagem_400px.png" id="img_animes" title="Todos os Animes que já assisti"></a>
         </div>
     </main>
     <?php include('rodape.php'); ?>
