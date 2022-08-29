7f2ab2b71ad6d842952807477ebd59a573ee2133
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-08-29 18:39:22 -0300

    updates no index, navbarra, CSS

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<?php session_start();
+include('conecta.php'); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,54 +11,48 @@
     <meta http-equiv="pragma" content="no-cache">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dtudo 2022</title>
-    <!-- <link rel="stylesheet" type="text/css" href="css/indexStyle.css"> -->
-    <!-- <link rel="stylesheet" type="text/css" href="css/geralStyle.css"> -->
-    <link rel="stylesheet" type="text/css" href="css/StyleGeral.css">
     <!-- AWESOME-FONTs com a CDNjs -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <!-- Link do Google Fonts (Cinzel+Decorative, Cutive+Mono, Kalam, Ubuntu)-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> TESTE, acho q não é necessária -->
     <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700&family=Cutive+Mono&family=Kalam:wght@300;400&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- StyleGeral.css - CSS local e geral para todas as paginas -->
+    <link rel="stylesheet" type="text/css" href="css/StyleGeral.css">
     <!-- Meu favicon atual -->
     <link rel="icon" sizes="128x128" href="imgs/favicon.ico">
 </head>
+
 <body>
-<div class="grid-conteiner_index"><!--GRID CONTEINER -->
-<?php session_start();
-  	  include('conecta.php');
-      include('navbarra.php');/* GRID ITEM, pagina externa navbarra.php */
-?>  
-<header id="header_cabec"><!-- GRID ITEM cabecalho -->
-        <div id="div_text_esq">
+    <!-- navbarra.php - Pagina externa -->
+    <?php include('navbarra.php'); ?>
+    <!-- HEADER ID=header - Cabeçalho da página-->
+    <header id="header">
+        <div id="div_esq">
             <img id="img_logo_grand" src="imgs/Logo_Dtudo_2022-300p.png" alt="Logo Dtudo" title="Imagem do Logo Dtudo">
         </div>
-        <div id="div_text_dir">
+        <div id="div_dir">
             Mais Informações estaram aqui!<br>
             Uma breve descrição do site!
         </div>
-</header>
-<main id="main_princ"><!--GRID ITEM e também um GRID CONTEINER-->
-<!--    <img id="img_fundo_main" src="imgs/software_1200px.jpg">-->
-        <div class="divs_main" id="div_img_centro" >
+    </header>
+    <main id="main_princ">
+        <!--GRID ITEM e também um GRID CONTEINER-->
+        <!--    <img id="img_fundo_main" src="imgs/software_1200px.jpg">-->
+        <div id="div_money" class="div_money">
             <a href="https://localhost/dtudo/bitcoin.php" target="_new" title="Pagina sobre Bitcoin">
-            <img src="imgs/SlaveMoney.png" id="img_bitcoin" alt="Escravo do Dinheiro" title="Informação Sobre como ganhar dinheiro"></a>
+                <img id="SlaveMoney" src="imgs/SlaveMoney.png" alt="Escravo do Dinheiro" title="Informação Sobre como ganhar dinheiro"></a>
         </div>
-        <div class="divs_main" id="div_img_bitcoin" onMouseOver="mudaFoto('imgs/bitcoin_text_centro.png')" onMouseOut="mudaFoto('imgs/terra_250px.png')" >
-        </div>
-        <div class="divs_main" id="div_img_ti" onMouseOver="mudaFoto('imgs/TI_text_centro.png')" onMouseOut="mudaFoto('imgs/terra_250px.png')">
+        <div class="divs_main" id="div_img_ti">
             <a href="https://localhost/dtudo/t_i.php" target="_new" title="Pagina sobre Tecnologia da Informação">
                 <img src="imgs/TI_link.png" id="img_ti" alt="T.I" title="Informação sobre Tecnologia da Informação"></a>
         </div>
-        <div class="divs_main" id="div_img_musicas" onMouseOver="mudaFoto('imgs/musica_text_centro.png')" onMouseOut="mudaFoto('imgs/terra_250px.png')">
+        <div class="div_" id="div_img_musicas">
             <a href="https://localhost/dtudo/animacao/animacao.php" target="_new" title="Pagina sobre Animes em Geral">
                 <img src="imgs/animes_personagem_400px.png" id="img_animes" alt="Animes" title="Todos os Animes que já assisti"></a>
         </div>
     </main>
     <?php include('rodape.php'); ?>
-<!-- <script>
-    function mudaFoto (foto) { document.getElementById("img_centro").src=foto; }
-</script> -->
-</div>
 </body>
+
 </html>
--- navbarra.php
+++ navbarra.php
@@ -1,8 +1,4 @@
-<head>
-<!-- BOOTSTRAP CSS-->
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> -->
-    <!-- <link rel="stylesheet" type="text/css" href="css/navStyle.css"> -->
-</head>
+<link rel="stylesheet" type="text/css" href="css/NavBarra.css">
 <nav id="nav_barra">
     <div id="container">
         <div id="img_logo_peq">
@@ -10,40 +6,45 @@
                 <img class="img_logo_peq" src="imgs/Logo-Dtudo_30px.png" alt="Logo Dtudo" title="Logo Dtudo Pequeno">
             </a>
             <ul id="list_login">
-                <?php  if (empty($_SESSION['id'])) { ?>
+                <?php if (empty($_SESSION['id'])) { ?>
                     <a href="login.php" alt="Link Log" title="Link Log">
-                        <li class="li_logins">Fazer Login</li></a>
-                <?php } else {
-                    if($_SESSION['adm']==0) {
-                    $consulta_user=$conexao->query("SELECT nome FROM usuarios WHERE id_user='$_SESSION[id]'");
-                    $exibe_user=$consulta_user->fetch(PDO::FETCH_ASSOC);
-                ?>
-                    <a href="areaUser.php">
-                        <li class="li_logins"><?php echo $exibe_user['nome']; ?></li></a>
-                    <a href="sair.php">
-                        <li class="li_logins">Logout</li></a>
-                <?php } else { ?>
-                    <a href="adm.php">
-                        <li class="li_logins">NinoJP</li></a>
-                    <a href="sair.php">
-                        <li class="li_logins">Logout</li></a>
+                        <li class="li_logins">Fazer Login</li>
+                    </a>
+                    <?php } else {
+                    if ($_SESSION['adm'] == 0) {
+                        $consulta_user = $conexao->query("SELECT nome FROM usuarios WHERE id_user='$_SESSION[id]'");
+                        $exibe_user = $consulta_user->fetch(PDO::FETCH_ASSOC);
+                    ?>
+                        <a href="areaUser.php">
+                            <li class="li_logins"><?php echo $exibe_user['nome']; ?></li>
+                        </a>
+                        <a href="sair.php">
+                            <li class="li_logins">Logout</li>
+                        </a>
+                    <?php } else { ?>
+                        <a href="adm.php">
+                            <li class="li_logins">NinoJP</li>
+                        </a>
+                        <a href="sair.php">
+                            <li class="li_logins">Logout</li>
+                        </a>
                 <?php }
-                }?>
+                } ?>
             </ul>
         </div>
         <ul id="list_menu">
             <a class="meu_btn" href="https://localhost/dtudo/bitcoin.php" target="_new" title="Pagina sobre Bitcoin">
-                <li>$Ganhar$</li></a>
+                <li>$Ganhar$</li>
+            </a>
             <a class="meu_btn" href="https://localhost/dtudo/t_i.php" target="_new" title="Informção sobre T.I">
-                <li>T.I</li></a>
+                <li>T.I</li>
+            </a>
             <a class="meu_btn" href="https://localhost/dtudo/animacao/animacao.php" target="_new" title="Pagina sobre Animes em Geral">
-                <li>Animes</li></a>
+                <li>Animes</li>
+            </a>
         </ul>
         <div id="menu-btn">
             <i class="fa-solid fa-bars"></i>
         </div>
     </div>
 </nav>
-<!-- Bliblioteca JavaScrip do BOOTSTRAP
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script> -->
