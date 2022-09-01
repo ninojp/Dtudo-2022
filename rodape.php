57ef836f734ae17267dd100bb1bad2e1fc295b70
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-08-31 23:43:04 -0300

    updates no index e alterações no rodapé

--- rodape.php
+++ rodape.php
@@ -3,15 +3,12 @@
 </head>
     <footer id="footer_rodape">
         <div id="div_esquerda">
-            <p>🌎 Nino JP - Estudante de Tecnologia da informação a 23 anos 🌎</p>
+            <p>🌎 Em Desenvolvimento por: Edenilson - Nino JP 🌎</p>
         </div>
         <div id="div_centro">
-            <p>Em construção<img src="imgs/em_construcao.gif">desde 06 de maio 2021</p>
+            <img src="imgs/em_construcao.gif" alt="Gif, em construção" title="Gif, em construção"><p> Criado em 06 de maio 2021</p>
         </div>
         <div id="div_direita">
-            <p>Meu blog 2008...<a href="http:vejak.blogspot.com">Vejak.blogspot.com</a></p>
-        </div>
-        <div>
-        <a href="#main_princ">Topo da pagina <img src="imgs/PraCima.gif" id="pra_cima"> </a>
+            <a href="http:vejak.blogspot.com" target="_blank">Meu blog: Vejak</a>
         </div>
     </footer>
