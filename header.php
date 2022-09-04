54944f57a453b153e0afbae9ccf6169055870bed
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-09-03 21:33:34 -0300

    updates

--- header.php
+++ header.php
@@ -7,10 +7,10 @@
         </div>
     </div>
     <div id="row_header" class="row">
-        <div id="div_esq" class="col-xxl-5">
+        <div id="div_esq" class="col-xxl-4 col-xl-4">
             <img id="img_logo_grand" src="imgs/Logo_Dtudo_2022-300p.png" alt="Logo Dtudo" title="Imagem do Logo Dtudo">
         </div>
-        <div id="div_dir" class="col-xxl-7">
+        <div id="div_dir" class="col-xxl-8 col-xl-8">
             <p class="p_texto">Dtudo é um site informativo sobre T.I.<br>
             Um exemplo prático de que é possível sobreviver$ de T.I.<br>
             O caminho posso tentar mostrar. Percorrer é com você.<br>
