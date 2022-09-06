c5e4c69e3e5930282844d9adb263bcd2c7d82435
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-09-06 19:12:09 -0300

    arquivos q não estavam mais em USO, DELETADOs

--- enviarEmail.php
+++ enviarEmail.php
@@ -1,7 +1,7 @@
 <?php 
 include 'conexao.php';
 $recebe_email = $_POST['email'];
-$consulta = $conexao->query("SELECT nome,senha,email FROM usuarios WHERE email='$recebe_email'");
+$consulta = $conexao->query("SELECT nome,senha,email FROM usuario WHERE email='$recebe_email'");
 if ($consulta->rowCount()==0) {
 	header('location:erro2.php');
 } else {
