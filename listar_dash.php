05191dd36b2996a3e45baf119e9c4a00781b281d
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-09-12 22:30:35 -0300

    criando o listar_dash.php

--- listar_dash.php
+++ listar_dash.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dtudo 2022 - Pagina do Administrador</title>
+    <title>Listar</title>
     <!-- inclusão do FONT-AWESOME -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <!-- Meu CSS local adm.css -->
@@ -57,7 +57,7 @@
         <!-- Inicio do SIDE-BAR -->
         <div class="sidebar">
             <a href="index.php" class="sidebar_nav active"><i class="icon fa-solid fa-table-columns"></i><span>Dashboard</span></a>
-            <a href="listar_dash.php" class="sidebar_nav"><i class="icon fa-solid fa-users"></i><span>Listar</span></a>
+            <a href="listar.php" class="sidebar_nav"><i class="icon fa-solid fa-users"></i><span>Listar</span></a>
             <a href="login.php" class="sidebar_nav"><i class="icon fa-solid fa-person-walking-dashed-line-arrow-right"></i><span>Sair</span></a>
         </div>
         <!-- FIM do SIDE-BAR -->
@@ -65,26 +65,40 @@
         <!-- Inicio do conteudo do ADM -->
         <div class="wrapper">
             <div class="row">
-                <div class="box box_first">
-                    <span class="class=icon fa-solid fa-users"></span>
-                    <span>397</span>
-                    <span>Usuários</span>
+                <div class="top_list">
+                    <span class="title_content">Listar</span>
+                    <div class="top_list_right">Botão</div>
                 </div>
-                <div class="box box_second">
-                    <span class="fa-solid fa-truck-fast"></span>
-                    <span>43</span>
-                    <span>Entregas</span>
-                </div>
-                <div class="box box_third">
-                    <span class="fa-solid fa-circle-check"></span>
-                    <span>12</span>
-                    <span>Completas</span>
-                </div>
-                <div class="box box_fourth">
-                    <span class="fa-solid fa-triangle-exclamation"></span>
-                    <span>3</span>
-                    <span>Alertas</span>
-                </div>
+                <table class="table_list">
+                    <thead class="list_head">
+                        <tr>
+                            <th class="list_head_content">ID</th>
+                            <th class="list_head_content">Nome</th>
+                            <th class="list_head_content">E-mail</th>
+                            <th class="list_head_content">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody class="list_body">
+                        <tr>
+                            <td class="list_body_content">1</td>
+                            <td class="list_body_content">Cesar</td>
+                            <td class="list_body_content">cesar@celk.com.br</td>
+                            <td class="list_body_content">Visualizar Editar Apagar</td>
+                        </tr>
+                        <tr>
+                            <td class="list_body_content">2</td>
+                            <td class="list_body_content">Cesar2</td>
+                            <td class="list_body_content">cesar2@celk.com.br</td>
+                            <td class="list_body_content">Visualizar Editar Apagar</td>
+                        </tr>
+                        <tr>
+                            <td class="list_body_content">3</td>
+                            <td class="list_body_content">Cesar3</td>
+                            <td class="list_body_content">cesar3@celk.com.br</td>
+                            <td class="list_body_content">Visualizar Editar Apagar</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
         <!-- FIM do conteudo do ADM -->
