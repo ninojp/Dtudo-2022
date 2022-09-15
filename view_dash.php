b8931f87c21c1e888d1e3c1dde680805b9365972
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-09-14 22:35:58 -0300

    Criação do view_dash.php

--- view_dash.php
+++ view_dash.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dtudo 2022 - Pagina do Administrador</title>
+    <title>Dashboard - Visualizar</title>
     <!-- inclusão do FONT-AWESOME -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <!-- Meu CSS local adm.css -->
@@ -56,36 +56,48 @@
     <main class="main_content">
         <!-- Inicio do SIDE-BAR -->
         <div class="sidebar">
-            <a href="adm.php" class="sidebar_nav active"><i class="icon fa-solid fa-table-columns"></i><span>Dashboard</span></a>
+            <a href="adm.php" class="sidebar_nav"><i class="icon fa-solid fa-table-columns"></i><span>Dashboard</span></a>
             <a href="listar_dash.php" class="sidebar_nav"><i class="icon fa-solid fa-table-list"></i><span>Listar</span></a>
             <a href="formulario_dash.php" class="sidebar_nav"><i class="icon fa-brands fa-wpforms"></i><span>Formulário</span></a>
-            <a href="view_dash.php" class="sidebar_nav"><i class="icon fa-solid fa-eye"></i><span>Visualizar</span></a>
+            <a href="view_dash.php" class="sidebar_nav active"><i class="icon fa-solid fa-eye"></i><span>Visualizar</span></a>
             <a href="login.php" class="sidebar_nav"><i class="icon fa-solid fa-person-walking-dashed-line-arrow-right"></i><span>Sair</span></a>
         </div>
         <!-- FIM do SIDE-BAR -->
 
-        <!-- Inicio do conteudo do ADM -->
+        <!-- Inicio do conteudo do Visualizar ADM -->
         <div class="wrapper">
             <div class="row">
-                <div class="box box_first">
-                    <span class="class=icon fa-solid fa-users"></span>
-                    <span>397</span>
-                    <span>Usuários</span>
+                <div class="top_list">
+                    <span class="title_content"><h2>Dashboard - Visualizar</h2></span>
+                    <div class="top_list_right">
+                        <a href="listar_dash.php" class="btn_info" type="button">Listar</a>
+                    </div>
                 </div>
-                <div class="box box_second">
-                    <span class="fa-solid fa-truck-fast"></span>
-                    <span>43</span>
-                    <span>Entregas</span>
-                </div>
-                <div class="box box_third">
-                    <span class="fa-solid fa-circle-check"></span>
-                    <span>12</span>
-                    <span>Completas</span>
-                </div>
-                <div class="box box_fourth">
-                    <span class="fa-solid fa-triangle-exclamation"></span>
-                    <span>3</span>
-                    <span>Alertas</span>
+                <div class="content_adm">
+                    <div class="view_det">
+                        <span class="view_det_title">Nome:</span>
+                        <span class="view_det_info">Nino</span>
+                    </div> 
+                    <div class="view_det">
+                        <span class="view_det_title">E-mail:</span>
+                        <span class="view_det_info">nino@gmail.com</span>
+                    </div>
+                    <div class="view_det">
+                        <span class="view_det_title">Título:</span>
+                        <span class="view_det_info">Informações</span>
+                    </div>
+                    <div class="view_det">
+                        <span class="view_det_title">Título 1:</span>
+                        <span class="view_det_info">Informações 1</span>
+                    </div>
+                    <div class="view_det">
+                        <span class="view_det_title">Título 2:</span>
+                        <span class="view_det_info">Informações 2</span>
+                    </div>
+                    <div class="view_det">
+                        <span class="view_det_title">Título 3:</span>
+                        <span class="view_det_info">Informações 3</span>
+                    </div>
                 </div>
             </div>
         </div>
